412f9c2d0b11c834b8aacbbf0389fc4d6e61daa6
frankfarmer <frank@wikia-inc.com>
2017-03-20 15:42:50 -0700

    bump version number

--- example-daemon/build.sh
+++ example-daemon/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-../gradlew  build 
+../gradlew clean build
 mkdir -p pkg/etc/init/ pkg/opt/denominator/
 cp build/denominatord pkg/opt/denominator/
 cd pkg
-VER=1.0
+VER=1.4
 fpm -s dir -t deb -n denominator -v $VER .
