8ce28c8aaab2174b570975baa6ef326c717c3d2f
frankfarmer <frank@wikia-inc.com>
2017-03-20 15:39:04 -0700

    fix dir prefix

--- example-daemon/build.sh
+++ example-daemon/build.sh
@@ -2,5 +2,6 @@
 ../gradlew  build 
 mkdir -p pkg/etc/init/ pkg/opt/denominator/
 cp build/denominatord pkg/opt/denominator/
+cd pkg
 VER=1.0
-fpm -s dir -t deb -n denominator -v $VER pkg
+fpm -s dir -t deb -n denominator -v $VER .
